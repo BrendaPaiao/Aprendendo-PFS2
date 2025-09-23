e9001defc60efaedd63d6b758d6d4b219dc866ba
BrendaPaiao <brendamelop8@gmail.com>
2025-09-23 12:29:27 -0300

    Feat: Criando o usuarioRepository.js

---
+++ repositories/usuarioRepository.js
@@ -0,0 +1,16 @@
+//Importação da entidade Usuario (classe que representa a tabela de usuários no sistema)
+import Usuario from "../entities/usuarioEntity";
+
+//O Repository acessa o banco e converte os registros em objetos (molde, nesse caso, do usuário) da Entity
+export default class UsuarioRepository {
+
+    //Aqui iremos buscar todos os usuários no banco e devolver uma lista de objetos Usuario.
+    listar() {
+
+    }
+
+    //Aqui iremos salvar a entidade Usuario recebida no banco.
+    cadastrar(usuarioEntidade) {
+
+    }
+}
