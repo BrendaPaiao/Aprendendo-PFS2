adc07cac4ffb02764be4898421b769386db97a7a
BrendaPaiao <brendamelop8@gmail.com>
2025-09-24 13:05:24 -0300

    Docs: Mais correções nas rotas do usuário e swaggerOutput.js

--- routes/usuarioRoute.js
+++ routes/usuarioRoute.js
@@ -12,7 +12,7 @@ router.get("/", (req, res) => {
     // Define a resposta HTTP 404 desta rota
     // Usa o schema "erro" (definido em components)
 
-    // #swagger.tags = ['Usuários']     
+    // #swagger.tags = ['Usuário']     
     // #swagger.summary = 'Listar todos os usuários'      
 
     /* #swagger.responses[404] = {      
@@ -29,7 +29,7 @@ router.post("/", (req, res) => {
     // Tipo de conteúdo esperado
     // Usa o schema "usuario" definido em components
 
-    // #swagger.tags = ['Usuários']
+    // #swagger.tags = ['Usuário']
     // #swagger.summary = 'Cadastrar um novo usuário'
 
     /* #swagger.requestBody = {
@@ -48,21 +48,32 @@ router.post("/", (req, res) => {
 // Rota PUT /usuarios → chama o método atualizar do controller
 router.put("/", (req, res) => {
 
-    // #swagger.tags = ['Usuários']
+    // #swagger.tags = ['Usuário']
     // #swagger.summary = 'Altera um usuário existente'
+        /* #swagger.requestBody = {
+        required: true,
+        content: {
+            "application/json": {
+                schema: {
+                    $ref: '#/components/schemas/usuario'
+                }
+            }
+        }
+    }
+    */
     ctrl.atualizar(req, res);
 });
 // Rota DELETE /usuarios/:id → chama o método deletar do controller
 // OBS: deve ficar por último, porque se vier antes pode "interceptar" rotas parecidas (como /usuarios/algumaCoisa) e causar erro.
 router.delete("/:id", (req, res) => {
 
-    // #swagger.tags = ['Usuários']
+    // #swagger.tags = ['Usuário']
     // #swagger.summary = 'Deleta permanentemente um usuário'
     ctrl.deletar(req, res);
 });
 router.get("/:id", (req, res) => {
 
-    // #swagger.tags = ['Usuários']
+    // #swagger.tags = ['Usuário']
     // #swagger.summary = 'Recupera um usuário através de um ID'
     ctrl.listar(req, res);
 });
